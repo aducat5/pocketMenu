b026d07642b30880fb74414b02e39b6b58633c97
Tamer Berat Çelik <aducat5@hotmail.com>
2020-06-11 23:56:28 +0300

    map placeholder image added to map screen

--- views/MapScreen.js
+++ views/MapScreen.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 
 function MapScreen(props) {
     console.log(props);
     return (
         <View>
-            <View>
-                <Text>This is the MapScreen screen!</Text>
-            </View>
-            <View>
-                
-            </View>
+            <Image source={require('../assets/map.png')}></Image>
         </View>
         );
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        flexDirection:"column",
+        marginTop:20
+    }
+});
 
 export default MapScreen;
