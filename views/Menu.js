05a38252fb20e0d20fdb39515b4710b35d61229e
Tamer Berat Çelik <aducat5@hotmail.com>
2020-06-14 23:04:04 +0300

    open detail on item press feature added.

--- views/Menu.js
+++ views/Menu.js
@@ -1,176 +1,238 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text, View, Button, SectionList } from 'react-native';
+import { useState } from 'react'
+import { SafeAreaView, StyleSheet, Text, View, Image, SectionList } from 'react-native';
 import Constants from "expo-constants";
 import { Ionicons } from '@expo/vector-icons';
+import { TouchableOpacity, TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
-const DATA = [
-    {
-      title: "Main dishes",
-      data: [
-        {
-          productName: "Pizza",
-          desciption: "Açıklama açıklama lorem ipsum.",
-          price: "25₺",
-          imageUrl: "urlToImage"
-        },
-        {
-          productName: "Burger",
-          desciption: "Açıklama açıklama lorem ipsum.",
-          price: "25₺",
-          imageUrl: "urlToImage"
-        },
-        {
-          productName: "Risotto",
-          desciption: "Açıklama açıklama lorem ipsum.",
-          price: "25₺",
-          imageUrl: "urlToImage"
-        },
-      ]
-    },
-    {
-      title: "Desserts",
-      data: [
-        {
-          productName: "Cheese Cake",
-          desciption: "Açıklama açıklama lorem ipsum.",
-          price: "25₺",
-          imageUrl: "urlToImage"
-        },
-        {
-          productName: "Ice Cream",
-          desciption: "Açıklama açıklama lorem ipsum.",
-          price: "25₺",
-          imageUrl: "urlToImage"
-        }
-      ]
-    },
-    {
-      title: "Sides",
-      data: [
-        {
-          productName: "French Fries",
-          desciption: "Açıklama açıklama lorem ipsum.",
-          price: "25₺",
-          imageUrl: "urlToImage"
-        },
-        {
-          productName: "Onion Rings",
-          desciption: "Açıklama açıklama lorem ipsum.",
-          price: "25₺",
-          imageUrl: "urlToImage"
-        },
-        {
-          productName: "Fried Shrimps",
-          desciption: "Açıklama açıklama lorem ipsum.",
-          price: "25₺",
-          imageUrl: "urlToImage"
-        },
-      ]
-    },
-    {
-      title: "Drinks",
-      data: [
-        {
-          productName: "Water",
-          desciption: "Açıklama açıklama lorem ipsum.",
-          price: "25₺",
-          imageUrl: "urlToImage"
-        },
-        {
-          productName: "Coke",
-          desciption: "Açıklama açıklama lorem ipsum.",
-          price: "25₺",
-          imageUrl: "urlToImage"
-        },
-        {
-          productName: "Beer",
-          desciption: "Açıklama açıklama lorem ipsum.",
-          price: "25₺",
-          imageUrl: "urlToImage"
-        },
-      ]
-    }
-  ];
+function Menu(props) {
+  var menuId = props.route.params.menuId;
 
-  const Product = ({ name, desciption, price, image }) => (
-    <View style={style.product}>
-      <View style={style.productText}>
-        <View style={{flexDirection:"row"}}>
-          <Text style={style.productName}>{name}</Text>
-          <Text style={{marginLeft: 15, fontSize: 12, marginTop: 4}} >{price}</Text>
-        </View>
-        <Text style={style.productPrice}>{desciption}</Text>
-      </View>
-      <View style={{flex:0.5}} >
-      <Ionicons name="md-images" size={24} color="#FF5733" />
-      </View>
-    </View>
-  );
+  var DATA = {
+    restourantId: "1",
+    restourantName: "First Restourant",
+    menus: [
+      {
+        title: "Main dishes",
+        data: [
+          {
+            productName: "Pizza",
+            desciption: "Açıklama açıklama lorem ipsum.",
+            price: "25₺",
+            imageUrl: "urlToImage",
+            detailVisible: false
+          },
+          {
+            productName: "Burger",
+            desciption: "Açıklama açıklama lorem ipsum.",
+            price: "25₺",
+            imageUrl: "urlToImage",
+            detailVisible: false
+          },
+          {
+            productName: "Risotto",
+            desciption: "Açıklama açıklama lorem ipsum.",
+            price: "25₺",
+            imageUrl: "urlToImage",
+            detailVisible: false
+          },
+        ]
+      },
+      {
+        title: "Desserts",
+        data: [
+          {
+            productName: "Cheese Cake",
+            desciption: "Açıklama açıklama lorem ipsum.",
+            price: "25₺",
+            imageUrl: "urlToImage",
+            detailVisible: false
+          },
+          {
+            productName: "Ice Cream",
+            desciption: "Açıklama açıklama lorem ipsum.",
+            price: "25₺",
+            imageUrl: "urlToImage",
+            detailVisible: false
+          }
+        ]
+      },
+      {
+        title: "Sides",
+        data: [
+          {
+            productName: "French Fries",
+            desciption: "Açıklama açıklama lorem ipsum.",
+            price: "25₺",
+            imageUrl: "urlToImage",
+            detailVisible: false
+          },
+          {
+            productName: "Onion Rings",
+            desciption: "Açıklama açıklama lorem ipsum.",
+            price: "25₺",
+            imageUrl: "urlToImage",
+            detailVisible: false
+          },
+          {
+            productName: "Fried Shrimps",
+            desciption: "Açıklama açıklama lorem ipsum.",
+            price: "25₺",
+            imageUrl: "urlToImage",
+            detailVisible: false
+          },
+        ]
+      },
+      {
+        title: "Drinks",
+        data: [
+          {
+            productName: "Water",
+            desciption: "Açıklama açıklama lorem ipsum.",
+            price: "25₺",
+            imageUrl: "urlToImage",
+            detailVisible: false
+          },
+          {
+            productName: "Coke",
+            desciption: "Açıklama açıklama lorem ipsum.",
+            price: "25₺",
+            imageUrl: "urlToImage",
+            detailVisible: false
+          },
+          {
+            productName: "Beer",
+            desciption: "Açıklama açıklama lorem ipsum.",
+            price: "25₺",
+            imageUrl: "urlToImage",
+            detailVisible: false
+          },
+        ]
+      }
+    ]
+  };
   
+  const [menuData, setMenuData] = useState(DATA);
+  const [refresh, setRefresh] = useState(false);
+
+  function productOnPress(product) {
+    product.detailVisible = !product.detailVisible;
+    setMenuData(menuData);
+    setRefresh(!refresh);
+  }
+
+  const SeeDetailIcon = function ( {detailVisible} ) {
+    let iconName = "ios-arrow-forward";
+    let iconColor = "#FF5733"
+    if (detailVisible) {
+      iconName = "ios-arrow-down";
+      iconColor = "#202020";
+    }
+  
+    return (<Ionicons name={iconName} size={24} color={iconColor} />)
+  }
+
+  const ProductDetail = function ( {item} ) {
+    if(item.detailVisible){
+      return (
+      <View>
+        <Image source={require('../assets/product/drink.jpg')} />
+        <Text>{item.productName}</Text>
+        <Text>{item.desciption}</Text>
+        <Text>{item.price}</Text>
+      </View>
+      )
+    }
+
+    return (<View></View>)
+  }
+    
   const SectionHeader = ({ title }) => (
     <View style={style.sectionHeader}>
       <Text style={style.sectionText} >{title}</Text>
     </View>
   );
-
-  function Menu(props) {
-    var menuId = props.route.params.menuId;
-    return (
-      <View style={style.container}>
-        <SafeAreaView>
+  
+  const Product = ({ item, onPress }) => (
+    <View>
+      <TouchableOpacity onPress={() => onPress(item)}>
+        <View style={style.product}>
+          <View style={style.productText}>
+            <View style={{flexDirection:"row"}}>
+              <Text style={style.productName}>{item.productName}</Text>
+            </View>
+            {/* <Text style={style.productPrice}>{desciption}</Text> */}
+            <Text style={style.productPrice} >{item.price}</Text>
+          </View>
+          <View style={{flex:0.5}} >
+            <SeeDetailIcon detailVisible={item.detailVisible} />
+          </View>
+        </View>
+      </TouchableOpacity>
+      <TouchableWithoutFeedback onPress={() => onPress(item)}>
+        <ProductDetail item={item} />
+      </TouchableWithoutFeedback>
+    </View>
+  );
+  
+  return (
+    <View style={style.container}>
+      <SafeAreaView>
+        <View style={{borderBottomWidth: 2, borderBottomColor: "#E5E5E5", marginVertical:10}}>
+          <Text style={{fontSize: 22, marginBottom:10}}>{menuData.restourantName}</Text>
+        </View>
         <SectionList
-          sections={DATA}
+          sections={menuData.menus}
+          extraData={refresh}
           keyExtractor={(item, index) => item + index}
-          renderItem={({ item }) => <Product name={item.productName} price={item.price} image={item.image} desciption={item.desciption} />}
+          renderItem={({ item }) => <Product item={item} onPress={productOnPress} />}
           renderSectionHeader={({ section: { title } }) => ( <SectionHeader title={title} /> )}
         />
-        </SafeAreaView>
-      </View>
-      );
-  }
+      </SafeAreaView>
+    </View>
+    );
+}
 
-  const style = StyleSheet.create({
-    container:{
-      marginTop:25,
-      flex: 1,
-      marginTop: Constants.statusBarHeight,
-      marginHorizontal: 16
-    },
-    product: {
-      flex:6,
-      flexDirection:"row",
-      alignItems:"center",
-      justifyContent:"flex-start",
-      height:60,
-      borderTopColor:"#E5E5E5",
-      borderTopWidth:1,
-      backgroundColor:"#EEEEEE"
-    },
-    productText: {
-      marginLeft: 10,
-      justifyContent:"space-between",
-      flex:4
-    },
-    productName: {
-      fontSize: 16,
-      color:"#373737"
-    },
-    productPrice: {
-      fontSize: 12,
-      color:"#4F4F4F"
-      
-    },
-    sectionHeader: {
-      fontSize: 23,
-      marginVertical: 10,
-      borderBottomColor: "#FF5733",
-      borderBottomWidth: 2,
-      borderRadius: 5
-    },
-    sectionText: {
-      marginLeft: 5,
-      fontSize: 20
-    }
-  });
+const style = StyleSheet.create({
+  container:{
+    flex: 1,
+    marginTop: Constants.statusBarHeight,
+    marginHorizontal: 16
+  },
+  product: {
+    flex:6,
+    flexDirection:"row",
+    alignItems:"center",
+    justifyContent:"flex-start",
+    height:60,
+    borderTopColor:"#E5E5E5",
+    borderTopWidth:1,
+    backgroundColor:"#EEEEEE"
+  },
+  productText: {
+    marginLeft: 10,
+    justifyContent:"space-between",
+    flex:4
+  },
+  productName: {
+    fontSize: 16,
+    color:"#373737"
+  },
+  productPrice: {
+    fontSize: 12,
+    color:"#4F4F4F"
     
-  export default Menu;
+  },
+  sectionHeader: {
+    marginVertical: 10,
+    borderBottomColor: "#FF5733",
+    borderBottomWidth: 2,
+    borderRadius: 5
+  },
+  sectionText: {
+    marginLeft: 5,
+    fontSize: 20
+  }
+});
+    
+export default Menu;
--- views/MenuHistory.js
+++ views/MenuHistory.js
@@ -10,17 +10,20 @@ const lastScans = [
   {
     id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
     retourantName: 'First Restourant',
-    menuName: "Taksim - 5 January 2020"
+    location: "Taksim",
+    date: "5 January 2020"
   },
   {
     id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
     retourantName: 'Second Restourant',
-    menuName: "Kadıköy - 15 November 2019"
+    location: "Kadıköy",
+    date: "15 November 2019"
   },
   {
     id: '58694a0f-3da1-471f-bd96-145571e29d72',
     retourantName: 'Third Restourant',
-    menuName: "Tuzla - 23 April 2020 "
+    location: "Tuzla",
+    date: "23 April 2020"
   },
 ];
 
@@ -36,7 +39,7 @@ function ListItem({ item }){
       <View style={style.listItem}>
         <View style={style.listItemText}>
           <Text style={{flex:0.6, color:"#373737"}}>{item.retourantName}</Text>
-          <Text style={{fontSize:10, color:"#4F4F4F"}}>{item.menuName}</Text>
+          <Text style={{fontSize:10, color:"#4F4F4F"}}>{item.location} - {item.date}</Text>
         </View>
         <Ionicons name="ios-arrow-forward" size={24} color="#FF5733" style={style.listItemArrow} />
       </View>
